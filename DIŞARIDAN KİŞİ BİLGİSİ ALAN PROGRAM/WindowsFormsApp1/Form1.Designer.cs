a150070c105c62ed61ec873a57ba5bbe4a27c41f
yemrebarmanbek <82896678+yemrebarmanbek@users.noreply.github.com>
2022-07-09 01:25:26 +0300

    registration program

---
+++ DIŞARIDAN KİŞİ BİLGİSİ ALAN PROGRAM/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,361 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(16, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(132, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "TC KİMLİK NO";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(16, 34);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(114, 24);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "ADI SOYADI";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(16, 60);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(51, 24);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "YAŞI";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(16, 87);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(123, 24);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "MEZUNİYETİ";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(155, 6);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(140, 22);
+            this.textBox1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(155, 34);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(140, 22);
+            this.textBox2.TabIndex = 5;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(155, 62);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(140, 22);
+            this.textBox3.TabIndex = 6;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "LİSE",
+            "LİSANS",
+            "YÜKSEKLİSANS"});
+            this.comboBox1.Location = new System.Drawing.Point(155, 90);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(140, 24);
+            this.comboBox1.TabIndex = 7;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(333, 5);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 28);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "ARA";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.groupBox1.Location = new System.Drawing.Point(599, 6);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(267, 52);
+            this.groupBox1.TabIndex = 9;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "cinsiyet";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(3, 22);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(70, 24);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "erkek";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(134, 22);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(69, 24);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "kadın";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(544, 79);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(128, 24);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "DOĞUM YERİ";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label6.Location = new System.Drawing.Point(544, 103);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(79, 24);
+            this.label6.TabIndex = 3;
+            this.label6.Text = "TEL NO";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(681, 79);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(155, 22);
+            this.textBox4.TabIndex = 10;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(681, 103);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(155, 22);
+            this.textBox5.TabIndex = 11;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button2.Location = new System.Drawing.Point(12, 183);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 33);
+            this.button2.TabIndex = 12;
+            this.button2.Text = "EKLE";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button3.Location = new System.Drawing.Point(131, 183);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(123, 34);
+            this.button3.TabIndex = 13;
+            this.button3.Text = "SİL CHECBOX";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button4.Location = new System.Drawing.Point(276, 183);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(113, 35);
+            this.button4.TabIndex = 14;
+            this.button4.Text = "SİL NORMAL";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button5.Location = new System.Drawing.Point(419, 183);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(123, 34);
+            this.button5.TabIndex = 15;
+            this.button5.Text = "TÜMÜNÜ SİL";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button6.Location = new System.Drawing.Point(585, 183);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(98, 34);
+            this.button6.TabIndex = 16;
+            this.button6.Text = "YENİ KAYIT";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label7.Location = new System.Drawing.Point(730, 186);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(111, 20);
+            this.label7.TabIndex = 17;
+            this.label7.Text = "KAYIT SAYISI";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(851, 188);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(56, 17);
+            this.label8.TabIndex = 18;
+            this.label8.Text = "______";
+            // 
+            // listView1
+            // 
+            this.listView1.CheckBoxes = true;
+            this.listView1.GridLines = true;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(9, 252);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(905, 194);
+            this.listView1.TabIndex = 19;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(919, 450);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.ListView listView1;
+    }
+}
+
---
+++ DIŞARIDAN KİŞİ BİLGİSİ ALAN PROGRAM/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            listView1.Columns.Add("TC KİMLİK NO", 150);
+            listView1.Columns.Add("ADI SOYADI", 200);
+            listView1.Columns.Add("YAŞI", 50);
+            listView1.Columns.Add("MEZUNİYET", 150);
+            listView1.Columns.Add("CİNSİYET", 125);
+            listView1.Columns.Add("DOĞUM YERİ", 125);
+            listView1.Columns.Add("TELEFON NO", 130);
+            kayitsayisiyaz();
+        }
+
+            private void kayitsayisiyaz()
+            {
+                int kayitsayisi = listView1.Items.Count;  // kayitsayisi kısmına listenin satır sayısını ekledik.(satır sayısı)
+                label8.Text = Convert.ToString(kayitsayisi);
+            }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            string tc = "", adsoyad = "", yas = "", mezuniyet = "", cinsiyet = "", dogumyeri = "", telefon = "";
+            tc = textBox1.Text; adsoyad = textBox2.Text; yas = textBox3.Text; mezuniyet = comboBox1.Text;
+            dogumyeri = textBox4.Text; telefon = textBox5.Text;
+            if (radioButton1.Checked == true)
+            {
+                cinsiyet = radioButton1.Text;
+            }
+            else if (radioButton2.Checked == true)
+            {
+                cinsiyet = radioButton2.Text;
+            }
+
+            string[] bilgiler = { tc, adsoyad, yas, mezuniyet, cinsiyet, dogumyeri, telefon }; //bilgileri bir dizinin içine attık döngü yapabilmek için
+            bool aranankayitkontolu = false;// bunu yapmamızın sebebi aynı girilen bir tc kimlik bilgisi varsa true değeri dönsün ve hata versin false olarak tanımladık hata yokmuş gibi
+
+            for (int i = 0; i < listView1.Items.Count; i++) // items satır count miktar demek satır miktarını dongüye tuttuk.
+            {  // for döngüsü yapmamızın sebebi tc kimlik ifadesinin döngüsünü oluşturmak(girilen ifadelerin döngüsü)
+
+                if (listView1.Items[i].SubItems[0].Text == textBox1.Text)
+                {
+                    // bu if döngüsündeki amaç tc kimlik sütununu kontrol etmek için yazdık
+                    // items ifadesi satır demek(bunu döngüye aldık i=0dan başladı) subitems ifadesi sütun demek her zaman tc sütünunu kontrol edecek biçimde
+                    aranankayitkontolu = true;
+                    MessageBox.Show(textBox1.Text + "tc kimlik numarası mevcut kayıt oluşturamazsınız");
+
+                }
+            }
+            if (aranankayitkontolu == false) // eğer farklı bir tc ise
+            {
+                ListViewItem lst = new ListViewItem(bilgiler);  // yeni bir liste oluştururuz eğer tc önceki girilenlerden farklı ise
+                if (tc != "" && adsoyad != "" && yas != "" && mezuniyet != "" && cinsiyet != "" && dogumyeri != "" && telefon != "")
+                {
+                    // bu if döngüsü eğer ifademizde boşluk yok ise işlenir 
+                    // tüm ifadeler yazıldıysa listeyi bastırabiliriz
+                    listView1.Items.Add(lst);
+                }
+                else
+                {
+                    MessageBox.Show("girdiğiniz bilgilerde eksiklik var");
+                }
+            }
+            kayitsayisiyaz(); // kayitsayisini yazdırdığımız metodu yazdık
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+            // biz listbox özelliğinden checbox seçeneğini true ifadesine çevirmiştik.
+            //şimdide sil(checkbox ) ifadesi sayesinde satır ifadelere tıklanabilir seçenek olduğunda tıklayıp seçili ifadeyi silebiliyoruz
+
+            int secilensayisi = listView1.CheckedItems.Count; // checbox sayesinde kaç tane kişi sayısı olduğunu tanımladık.
+
+            foreach (ListViewItem secilikayitbilgisi in listView1.CheckedItems)
+            {
+                // döngü oluşturduk in (içindeki) yani checkboxtaki seçili kişilerin içinden seçilikayıtbilgisine döngü akar
+
+                secilikayitbilgisi.Remove();// remove=kaldırmak  seçili kişileri bu döngü sayesinde kaldırdık.
+            }
+            MessageBox.Show(secilensayisi.ToString() + "adet kayıt silindi"); //silinen kayıt sayısını yazdırdık kullanıcıya
+            kayitsayisiyaz();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            // kayıdı silerek silmek için tüm ifadelerin çıkmasını istiyorsak listbox ayarlar kısmnında 
+            // fullrowselect kısmını true yaparız
+
+            int secilensayisi = listView1.SelectedItems.Count; //sil(checkbox) ile aynı gibi sadece burda seçtiğimiz için selected diye yaptık
+
+            foreach (ListViewItem secilikayitbilgisi in listView1.SelectedItems)
+            {
+                secilikayitbilgisi.Remove();
+            }
+            MessageBox.Show(secilensayisi.ToString() + "adet kayıt silindi");
+            kayitsayisiyaz();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+
+            listView1.Items.Clear(); // tümünü sil butonu ile tümünü sildirdik
+            kayitsayisiyaz();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // ara butonuna kod yazıyoruz
+            // listview nesnesinde item satır subitem sütun demek
+            bool aranan_kayit_kontrolu = false;
+            for (int i = 0; i < listView1.Items.Count; i++)
+            {
+                if (listView1.Items[i].SubItems[0].Text==textBox1.Text) 
+                {  // girilen satır kadar bir döngü oluşturduk ve tc kimlik noları karşılaştırdık
+
+                    aranan_kayit_kontrolu = true;
+                    textBox2.Text = listView1.Items[i].SubItems[1].Text;
+                    textBox3.Text = listView1.Items[i].SubItems[2].Text;
+                    comboBox1.Text = listView1.Items[i].SubItems[3].Text;
+
+                    if (listView1.Items[i].SubItems[4].Text=="erkek")
+                    {
+                        radioButton1.Checked = true;
+                    }
+                    else if (listView1.Items[i].SubItems[4].Text == "bayan")
+                    {
+                        radioButton2.Checked = true;
+                    }
+
+                    textBox4.Text = listView1.Items[i].SubItems[5].Text;
+                    textBox5.Text = listView1.Items[i].SubItems[6].Text;
+
+                    // kişiyi aradıktan sonra nesneleri kullanılamaz hale getiriyoruz
+                    textBox2.Enabled = false; textBox3.Enabled = false; comboBox1.Enabled = false;
+                    groupBox1.Enabled = false; textBox4.Enabled = false; textBox5.Enabled = false;
+
+                }
+            }
+
+        }
+    }
+    }
+
